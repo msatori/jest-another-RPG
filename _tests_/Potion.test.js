ed1b94e245ff98fbea61a8897845bd3627838c32
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-03 00:15:10 -0600

    working potion test

---
+++ _tests_/Potion.test.js
@@ -0,0 +1,13 @@
+const { TestScheduler } = require('jest')
+const Potion = require('../lib/Potion')
+
+test('creates a health potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+
+});
+
+
